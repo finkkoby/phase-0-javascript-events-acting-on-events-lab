0a81dbee017206a1207f0acede4a3cdc1ba798a1
Koby Fink <finkkoby@icloud.com>
2024-02-05 16:49:57 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 // Your code here
-function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
+let dodger = document.querySelector("#dodger");
+function moveDodgerLeft(e) {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
     if (left > 0) {
       dodger.style.left = `${left - 1}px`;
     }
+}
+function moveDodgerRight(e) {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
   }
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowLeft") {
-      moveDodgerLeft();
-    }
-  });
-  function moveDodgerRight() {
-    const rightNumbers = dodger.style.left.replace("px", "");
-    const right = parseInt(rightNumbers, 10);
-  
-    if (right < 360) {
-      dodger.style.left = `${right + 1}px`;
-    }
+ }
+
+ document.addEventListener("keydown", function(e) {
+  if(e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if(e.key === "ArrowRight") {
+    moveDodgerRight();
   }
-  document.addEventListener("keydown", function (event) {
-    if (event.key === "ArrowRight") {
-        moveDodgerRight();
-    }
-  });
+ });
+ document.addEventListener("keydown", moveDodgerLeft);
